50df09931dd0c12dcd7bde1c8b6b93b2debedeb3
ahmednkhan24 <ahmednkhan24@gmail.com>
2024-06-26 21:27:44 -0500

    Saving

--- loopindex.py
+++ loopindex.py
@@ -1,14 +1,34 @@
 import sys
 
+# Grab the list from the command line 
+# Input: `python loopindex.py 1 2 3` -> loop_list = ['1', '2', '3']
 loop_list = sys.argv[1:]
 
 def loop_index():
-  result = []
+  # loop through every index of loop_list
+  # Ex: loop_list = ['1', '2', '3']
+  # len(loop_list) = 3
+  # range(len(loop_list)) -> loop 3 times, 
+  # where `i` = 0 on loop 1
+  # where `i` = 1 on loop 2
+  # where `i` = 2 on loop 3
   for i in range(len(loop_list)):
+    # if loop_list = ['1', '2', '3'], then loop_list[0] = '1', loop_list[1] = '2', etc
     num_str = loop_list[i]
-    num_int = int(num_str)
-    value = num_int + i
-    result.append(value)
-  print(result)
 
+    # convert the string to an integer. 
+    # '2' is of type str, but 2 is of type int
+    num_int = int(num_str)
+
+    # add the index to the number in the array
+    # if loop_list = ['1', '2', '3']
+    # where `i` = 0 on loop 1, then 1 + 0 = 1
+    # where `i` = 1 on loop 2, then 2 + 1 = 3
+    # where `i` = 2 on loop 3, then 3 + 2 = 5
+    value = num_int + i
+
+    # add the result to the final array
+    print(result)
+
+# invoke the function we defined above
 loop_index()
--- reallyrandom.py
+++ reallyrandom.py
@@ -4,18 +4,17 @@ import numpy as np
 np.random.seed(42)
 
 def really_random(size_str, multiply_str, index_str):
-  print(size_str, multiply_str, index_str)
+  # convert the strings to integers
   size = int(size_str)
   multiply = int(multiply_str)
   index = int(index_str)
 
+  # don't do anything if the index is beyond the array size, or else the program will crash
   if index < size:
+    # create an array of `size` of random numbers between 0 and 10
     random_numbers = np.random.randint(0, 10, size=size)
-    print("random_numbers: ", random_numbers)
 
-    # multiplied = np.multiply(random_numbers, multiply)
     multiplied = random_numbers * multiply
-    print('multiplied: ', multiplied)
 
     result = multiplied[index]
     print("Your random value is", result)
